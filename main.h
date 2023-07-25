0a1b422df377fb39c22ac4b2d5574e349809bd75
yassin mahmoud <yassinmahmoudmostafa@gmail.com>
2023-07-25 05:56:03 -0700

    main.h

--- main.h
+++ main.h
@@ -18,13 +18,4 @@ void handle_character(char buffer[], int *buff_ind, int *count, char c);
 void handle_string(char buffer[], int *buff_ind, int *count, char *s);
 void handle_percent(char buffer[], int *buff_ind, int *count);
 void handle_integer(char buffer[], int *buff_ind, int *count, int num);
-void handle_binary(char buffer[], int *buff_ind, int *count, unsigned int num);
-void handle_octal(char buffer[], int *buff_ind, int *count, unsigned int num);
-void handle_hexadecimal(char buffer[], int *buff_ind, int *count, unsigned int num, bool uppercase);
-void handle_unsigned(char buffer[], int *buff_ind, int *count, unsigned int num);
-void handle_string_upper(char buffer[], int *buff_ind, int *count, char *s);
-void handle_pointer(char buffer[], int *buff_ind, int *count, void *ptr);
-void handle_reverse(char buffer[], int *buff_ind, int *count, char *s);
-void handle_rot13(char buffer[], int *buff_ind, int *count, char *s);
-
 #endif
