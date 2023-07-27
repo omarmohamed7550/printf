0d7182436cf8362d526369a45a29683982b95edf
Omar Abdelfattah <omaromar1234641@gmail.com>
2023-07-27 02:33:44 -0700

    task

--- _printf.c
+++ _printf.c
@@ -21,7 +21,9 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
+
 	va_start(ptr, format);
+
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -31,7 +33,10 @@ int _printf(const char *format, ...)
 			{
 				print_buffer(buffer, &buff_ind);
 			}
-			write(1, &format[i], 1);
+			else
+			{
+				write(1, &format[i], 1);
+			}
 			count++;
 		}
 		else
@@ -40,41 +45,74 @@ int _printf(const char *format, ...)
 			if (format[i] == 'c')
 			{
 				char c = (char)va_arg(ptr, int);
-
-				handle_character(buffer, &buff_ind, &count, c);
+				buffer[buff_ind++] = c;
+				if (buff_ind == BUFF_SIZE)
+				{
+					print_buffer(buffer, &buff_ind);
+				}
+				else
+				{
+					write(1, &c, 1);
+				}
+				count++;
 			}
 			else if (format[i] == 's')
 			{
-				char s = va_arg(ptr, char);
-
+				char *s = va_arg(ptr, char*);
 				if (s == NULL)
 				{
 					return (-1);
 				}
-				handle_string(buffer, &buff_ind, &count, s);
+				while (*s)
+				{
+					buffer[buff_ind++] = *s;
+					if (buff_ind == BUFF_SIZE)
+					{
+						print_buffer(buffer, &buff_ind);
+					}
+					else
+					{
+						write(1, s, 1);
+					}
+					count++;
+					s++;
+				}
 			}
 			else if (format[i] == '%')
 			{
-				handle_percent(buffer, &buff_ind, &count);
+				buffer[buff_ind++] = '%';
+				if (buff_ind == BUFF_SIZE)
+				{
+					print_buffer(buffer, &buff_ind);
+				}
+				else
+				{
+					write(1, "%", 1);
+				}
+				count++;
 			}
 		}
-
 	}
+
 	count += buff_ind;
+	print_buffer(buffer, &buff_ind); // Print any remaining characters in the buffer
 	va_end(ptr);
 	return (count);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer : is a param
- * @buff_ind : is a param
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: is a param
+ * @buff_ind: is a param
  * Return : nothing
  */
 
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
+	{
 		write(1, buffer, *buff_ind);
+	}
+
 	*buff_ind = 0;
 }
